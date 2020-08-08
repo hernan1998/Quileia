ebd00383eee85a89bdf60cea7be25b5561f684de
Hernan Parra <hernandre1122@gmail.com>
2020-08-07 21:53:29 -0500

    Vista restaurantes y menus añadida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { RestaurantesComponent } from './pages/restaurantes/restaurantes.component';
+import { MenusComponent } from './pages/menus/menus.component';
+
+
 
 
 
 const routes: Routes = [
   { path: 'Home', component: HomeComponent },
+  { path: 'Home/Restaurantes', component: RestaurantesComponent },
+  { path: 'Home/Menus', component: MenusComponent },
   { path: '', redirectTo: '/Home', pathMatch: 'full' },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ModalModule } from '@independer/ng-modal';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { RestaurantesComponent } from './pages/restaurantes/restaurantes.component';
+import { MenusComponent } from './pages/menus/menus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    RestaurantesComponent,
+    MenusComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ModalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
